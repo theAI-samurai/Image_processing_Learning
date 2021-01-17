7f01d0a4eb9954c00b20af2d3fb512b1ced41715
Ankit Mishra <ankitmishra723@gmail.com>
2021-01-18 00:16:06 +0530

    ffmpeg command notes

--- Install_opencv.sh
+++ Install_opencv.sh
@@ -1,4 +1,4 @@
-OPENCV_VERSION='3.4.2'
+OPENCV_VERSION='3.4.5'
 sudo apt-get -y update
 sudo apt-get -y upgrade      
 sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
